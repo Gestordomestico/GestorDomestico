f13da19ebfde8d8de1837230260ec894af581fbc
sarrieta <sarrieta@zeustecnologia.com>
2025-06-11 11:17:05 -0500

    feat: Implement full Dockerization for Laravel on Render (PHP 8.3, PostgreSQL, auto-migrations)

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-php-fpm &
+# Inicia PHP-FPM escuchando en un socket Unix
+php-fpm -F --pid /var/run/php-fpm.pid --fpm-config /usr/local/etc/php-fpm.conf --daemonize no &
+
+# Permite un pequeño tiempo para que PHP-FPM inicie el socket
+sleep 5
+
+# Inicia Nginx en primer plano
 nginx -g "daemon off;"
